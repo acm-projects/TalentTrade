8a260cd177a5f05398fd5abce7443ea7ce5c2c48
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-09-17 00:21:41 -0500

    added mongodb and connected server to db

--- Blizzyfile.js
+++ Blizzyfile.js
@@ -1,15 +1,23 @@
 const express=require('express')
 const app=express()    //express app
-
+const mongoose=require('mongoose')
 
 //get request for browser
 app.get('/',(req,res)=>{
   res.json({msg:"Welcome to the browser"})
 })
 
+//connect to db
+mongoose.connect("mongodb+srv://shehaansunay:Sunay123@profileschema.5isd0.mongodb.net/")
+.then(()=>{
+  app.listen(4000,()=>{
+    console.log('Connected to DB and Listening on port',4000)
+  })
+})
+.catch((error)=>{
+  console.log(error)
+})
+
 
 
 //listening to request
-app.listen(4000,()=>{
-  console.log("Listening on port 4000")
-})
