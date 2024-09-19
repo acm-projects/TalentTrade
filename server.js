8a3dd7d6589de35486fad31c388f87c26eb3d595
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-09-19 17:25:06 -0500

    async and await

--- server.js
+++ server.js
@@ -9,18 +9,19 @@ app.get('/',(req,res)=>{
   res.json({"From the backend side":"Welcome to TalentTrade!"})
 })
 
-//connect to db
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-  app.listen(process.env.PORT,()=>{
-    console.log('Connected to DB and Listening on port',process.env.PORT)
-  })
-})
-.catch((error)=>{
-  console.log(error)
-})
 
+const connectToMongoose = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to DB and Listening on port',process.env.PORT)
+    })
+  } catch (error) {
+    console.log("error")
+  }
+}
 
+connectToMongoose()
 
 //listening to request
 
