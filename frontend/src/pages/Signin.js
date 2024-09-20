e0a25a90066809c8bb9125023d183780af851b32
avt05 <adrianvtran@Adrians-MacBook-Air.local>
2024-09-20 00:47:41 -0500

    Began signin page with styling

---
+++ frontend/src/pages/Signin.js
@@ -0,0 +1,16 @@
+const Signin = () => {
+    return (
+        <div className="signin">
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}></div>
+            <h1>Welcome!</h1>
+            <h2>Sign in to connect and trade<br></br>
+            skills with students.</h2>
+        </div>
+    )
+}
+
+export default Signin
