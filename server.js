b654d8730fe70ed1daba43e5049bf1ed44b247a5
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2024-09-20 03:25:06 -0500

    firebase backend

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 require ('dotenv').config()
 
-const express=require('express')
-const app=express()    //express app
-const mongoose=require('mongoose')
+const express = require('express')
+const mongoose = require('mongoose')
+const admin = require ("./firebaseAdmin")
+const cors = require("cors")
+
+const app = express()    //express app
+
+// cors for getting resources from external server
+app.use(cors());
 
 //get request for browser
 app.get('/',(req,res)=>{
@@ -23,6 +29,36 @@ const connectToMongoose = async () => {
 
 connectToMongoose()
 
-//listening to request
 
-//testing
+// verify id token, if it it not verified error is sent to client
+async function verifyToken (req, res, next) {
+  const idToken = req.headers.authorization;
+
+  if (!idToken) {
+    return res.status(401).send("Unauthorized Login");
+  }
+
+  try {
+    const decodedToken = await admin.auth().verfiyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).send("Unauthorized Login")
+  }
+}
+
+// post request (post user data)
+app.post("/api/users", verifyToken, async (req, res) => {
+  const { uid, name, email, picture } = req.user;
+
+  let user = await User.findne({ uid });
+
+  // if not user create one
+  if (!user) {
+    user = new User({ uid, name, email, picture})
+    await user.save();
+  }
+
+  // if user send as a response
+  res.send(user);
+});
