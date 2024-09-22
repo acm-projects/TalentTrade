892052bed0c3ac0ebed8ccdca5fae5fd8806c96a
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-09-21 20:36:09 -0500

    added router to handle requests

---
+++ router.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const UserProfile=require('./schema')
+
+const router=express.Router()
+
+router.get('/:id',(req,res)=>{
+  res.json({msg:"GET a user profile"})
+
+})
+
+router.post('/', async (req,res)=>{
+
+  try{
+    const {User}=req.body;
+    console.log(User)
+    const Userdata= await UserProfile.create({User})
+    res.status(200).json({Userdata})
+
+  }catch(error){
+    res.status(400).json({error:error.message})
+
+  }
+
+})
+
+
+router.delete('/:id',(req,res)=>{
+  res.json({msg:"DELETE a user profile"})
+
+})
+
+router.patch('/:id',(req,res)=>{
+  res.json({msg:"UPDATE a user profile"})
+
+})
+
+module.exports=router
+
+
