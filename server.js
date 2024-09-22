0461b33b130768562167b4d936f9b9614cac0b87
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-09-21 21:49:47 -0500

    imported router

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ require ('dotenv').config()
 const express=require('express')
 const app=express()    //express app
 const mongoose=require('mongoose')
+const userRoutes=require('./router')
+
+app.use(express.json()) //middleware
 
 //get request for browser
 app.get('/',(req,res)=>{
   res.json({"From the backend side":"Welcome to TalentTrade!"})
 })
 
+
+app.use('/api/users',userRoutes)
 
 const connectToMongoose = async () => {
   try {
