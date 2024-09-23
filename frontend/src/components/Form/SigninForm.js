1306a440b34e83a74e9a4ed25ff66f72df21f669
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-09-23 14:47:51 -0500

    added sign in user authentication

--- frontend/src/components/Form/SigninForm.js
+++ frontend/src/components/Form/SigninForm.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { firebaseConfig } from './firebaseauth';
+import { initializeApp } from 'firebase/app';
 import { Link } from 'react-router-dom'
 import './SigninForm.css'
+import{getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
 
 function SigninForm() {
+    const navigate=useNavigate()
+
+    const [emailinput,setemailinput]=useState('')
+    const[passwordinput,setpasswordinput]=useState('')
+
+    const handleemail=(event)=>{
+        setemailinput(event.target.value)
+    }
+    const handlepassword=(event)=>{
+        setpasswordinput(event.target.value)
+    }
+    const handlebuttonclick=()=>{
+        const app=initializeApp(firebaseConfig)
+        const auth=getAuth()
+        const db=getFirestore()
+
+        signInWithEmailAndPassword(auth,emailinput,passwordinput)
+        .then((userCredential)=>{
+            console.log("Successfully logged in");
+            const user=userCredential.user;
+            localStorage.setItem('loggedInUserId',user.uid);
+            navigate('/');
+})
+        .catch((error)=>{
+            const errorCode=error.code;
+            if(errorCode==="auth/invalid-credential"){
+            console.log("Incorrect email or password")
+            }
+            else{
+            console.log("Account does not exist");
+            }
+  })
+        
+    }
+
+
     return (
         <div className='container'>
             <div className="header">
@@ -10,14 +52,14 @@ function SigninForm() {
             </div>
             <div className="inputs">
                 <div className="input">
-                    <input type="email" id="email" placeholder="Email" />
+                    <input type="email" id="email" onChange={handleemail} placeholder="Email" />
                 </div>
                 <div className="input">
-                    <input type="password" id="password" placeholder="Password" />
+                    <input type="password" id="password" onChange={handlepassword} placeholder="Password" />
                 </div>
             </div>
             <div className="submit-container">
-                <div className="submit">Sign in</div>
+                <div className="submit" onClick={handlebuttonclick}>Sign in</div>
             </div>
             <div className="text">
                 <div className="redirect">
