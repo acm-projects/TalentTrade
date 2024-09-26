0175c2e808bf1432ee4bba455f932b47fb99eb15
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro-2.local>
2024-09-25 21:16:47 -0500

    Lot of stuff

--- frontend/src/components/Form/firebaseauth.js
+++ frontend/src/components/Form/firebaseauth.js
@@ -1,11 +1,9 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyAI7MV7tSiA3RcjfcD5fptou2HlWb2xgSk",
-  authDomain: "talent-trade-bbb32.firebaseapp.com",
-  projectId: "talent-trade-bbb32",
-  storageBucket: "talent-trade-bbb32.appspot.com",
-  messagingSenderId: "965531803567",
-  appId: "1:965531803567:web:9f26211445cb9c440fd95a",
-  measurementId: "G-07KGFH6JZD"
-};
-
+    apiKey: "AIzaSyAI7MV7tSiA3RcjfcD5fptou2HlWb2xgSk",
+    authDomain: "talent-trade-bbb32.firebaseapp.com",
+    projectId: "talent-trade-bbb32",
+    storageBucket: "talent-trade-bbb32.appspot.com",
+    messagingSenderId: "965531803567",
+    appId: "1:965531803567:web:9f26211445cb9c440fd95a",
+    measurementId: "G-07KGFH6JZD"
+  };
