7a829866596e93e7e8f0aa9165bdfc025e542027
danielnguywn <ddanielnnguyen17@gmail.com>
2024-09-28 04:12:46 -0500

    post reqeust signup, optional username

--- frontend/src/components/Form/SigninForm.js
+++ frontend/src/components/Form/SigninForm.js
@@ -1,79 +1,49 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { firebaseConfig } from './firebaseauth';
 import { initializeApp } from 'firebase/app';
-import './SigninForm.css';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { Link } from 'react-router-dom'
+import './SigninForm.css'
+import{getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
 
 function SigninForm() {
-    const navigate = useNavigate();
-    const [emailinput, setemailinput] = useState('');
-    const [passwordinput, setpasswordinput] = useState('');
+    const navigate=useNavigate()
 
-    const handleemail = (event) => {
-        setemailinput(event.target.value);
-    };
+    const [emailinput,setemailinput]=useState('')
+    const[passwordinput,setpasswordinput]=useState('')
 
-    const handlepassword = (event) => {
-        setpasswordinput(event.target.value);
-    };
+    const handleemail=(event)=>{
+        setemailinput(event.target.value)
+    }
+    const handlepassword=(event)=>{
+        setpasswordinput(event.target.value)
+    }
+    const handlebuttonclick=()=>{
+        const app=initializeApp(firebaseConfig)
+        const auth=getAuth()
+        const db=getFirestore()
 
-    const handlebuttonclick = () => {
-        const app = initializeApp(firebaseConfig);
-        const auth = getAuth();
-        const db = getFirestore();
+        signInWithEmailAndPassword(auth,emailinput,passwordinput)
+        .then((userCredential)=>{
+            console.log("Successfully logged in");
+            const user=userCredential.user;
+            localStorage.setItem('loggedInUserId',user.uid);
+            navigate('/');
+})
+        .catch((error)=>{
+            const errorCode=error.code;
+            if(errorCode==="auth/invalid-credential"){
+            console.log("Incorrect email or password")
+            }
+            else{
+            console.log("Account does not exist");
+            }
+  })
+        
+    }
 
-        signInWithEmailAndPassword(auth, emailinput, passwordinput)
-            .then((userCredential) => {
-                console.log("Successfully logged in");
-                const user = userCredential.user;
-                localStorage.setItem('loggedInUserId', user.uid);
-
-                // after auth, post req to create user (empty arrays for skills)
-                const userData = {
-                    User: {
-                        Personal_info: {
-                            Email: emailinput,
-                            Password: passwordinput,
-                        },
-                        Skills: {
-                            teaching_skills: [],
-                            learning_skills: []
-                        }
-                    }
-                };
-
-                fetch('http://localhost:4000/api/users', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(userData),
-                })
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error('Failed to create user');
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log("User created successfully:", data);
-                    navigate('/');
-                })
-                .catch((error) => {
-                    console.error("Error creating user:", error.message);
-                });
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                if (errorCode === "auth/invalid-credential") {
-                    console.log("Incorrect email or password");
-                } else {
-                    console.log("Account does not exist");
-                }
-            });
-    };
 
     return (
         <div className='container'>
@@ -93,7 +63,7 @@ function SigninForm() {
             </div>
             <div className="text">
                 <div className="redirect">
-                    <span>Don't have an account? <Link to="/signup" className="link">Sign up</Link></span>
+                    <span>Don't have an account? <Link to="/signup" class="link">Sign up</Link></span>
                 </div>
             </div>
         </div>
