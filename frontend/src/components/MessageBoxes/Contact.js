76ef44be5c39f8ba54c1732722d74c539054d3e6
avt05 <adrianvtran@gmail.com>
2024-09-30 23:42:25 -0500

    Worked more on messages page

---
+++ frontend/src/components/MessageBoxes/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+    return (
+        <div>
+            <div className="contact">
+                <img src={"/images/user.png"} class="small-profile-picture" />
+                <h3>Larry Jerry</h3><div className="indicator"></div>
+            </div>
+            <div className="selected-contact">
+                <img src={"/images/user.png"} class="small-profile-picture" />
+                <h3>Steve Jones</h3>
+            </div>
+            <div className="contact">
+                <img src={"/images/user.png"} class="small-profile-picture" />
+                <h3>Bob Robertson</h3>
+            </div>
+            <div className="contact">
+                <img src={"/images/user.png"} class="small-profile-picture" />
+                <h3>Saul Goodman</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
