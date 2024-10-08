9f470437a5afdd3a9c3850166b07f79a6156743c
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-10-08 18:02:58 -0500

    restored original file

--- frontend/src/components/Form/SigninForm.js
+++ frontend/src/components/Form/SigninForm.js
@@ -9,34 +9,9 @@ import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvide
 
 
 function SigninForm() {
-<<<<<<< HEAD
-    const fetchUserProfileByEmail = async (email) => {
-        try {
-            const response = await fetch(`http://localhost:4000/api/users/email/${encodeURIComponent(email)}`, {
-              method: 'GET',
-              credentials: 'include',
-            });
-        
-            const userProfile = await response.json();
-            console.log(`Fetching user profile from: http://localhost:4000/api/users/email/${encodeURIComponent(email)}`);
-
-        
-            if (response.ok) {
-              console.log('User profile data:', userProfile);
-            } else {
-                console.log(response)
-              console.log('Error fetching user profile:', userProfile.message || 'Unknown error');
-            }
-          } catch (error) {
-            console.error('Error fetching user profile:', error.message || error);
-          }
-        };
-    const navigate=useNavigate()
-=======
     const navigate = useNavigate();
     const [emailinput, setemailinput] = useState('');
     const [passwordinput, setpasswordinput] = useState('');
->>>>>>> 76ef44be5c39f8ba54c1732722d74c539054d3e6
 
     const handleemail = (event) => {
         setemailinput(event.target.value);
@@ -48,43 +23,6 @@ function SigninForm() {
         const app = initializeApp(firebaseConfig);
         const auth = getAuth();
 
-<<<<<<< HEAD
-    const handleemail=(event)=>{
-        setemailinput(event.target.value)
-    }
-    const handlepassword=(event)=>{
-        setpasswordinput(event.target.value)
-    }
-    const handlebuttonclick=()=>{
-        const app=initializeApp(firebaseConfig)
-        const auth=getAuth()
-        //const db=getFirestore()
-
-        signInWithEmailAndPassword(auth,emailinput,passwordinput)
-        .then((userCredential)=>{
-            console.log("Successfully logged in");
-            const user=auth.currentUser;
-            const mongodb = context.services.get("mongodb-atlas");
-            const db = mongodb.db("userprofiles");
-            user= db.users.find({userName:"Blizzy"})
-            console.log(user._id.tostring())
-            localStorage.setItem('loggedInUserId',user.uid);
-            navigate('/');
-
-            fetchUserProfileByEmail(emailinput)
-})
-        .catch((error)=>{
-            const errorCode=error.code;
-            if(errorCode==="auth/invalid-credential"){
-            console.log("Incorrect email or password")
-            }
-            else{
-            console.log("Account does not exist");
-            }
-  })
-        
-    }
-=======
         signInWithEmailAndPassword(auth, emailinput, passwordinput)
             .then((userCredential) => {
                 console.log("Successfully logged in");
@@ -106,7 +44,6 @@ function SigninForm() {
         const app = initializeApp(firebaseConfig);
         const auth = getAuth();
         const provider = new GoogleAuthProvider();
->>>>>>> 76ef44be5c39f8ba54c1732722d74c539054d3e6
 
         signInWithPopup(auth, provider)
             .then((result) => {
