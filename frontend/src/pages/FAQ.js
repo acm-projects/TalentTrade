f7875cf45916f4feccf4a45cc14d7fa1f2f4d6fc
Cheryl Wang <whxcollege@gmail.com>
2024-11-07 13:30:03 -0600

    faq navbar based on logged in

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -6,30 +6,31 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from 'react'
 
 const FAQ = () => {
-    // const auth = getAuth()
-    // const [loggedIn, setLoggedIn] = useState(Boolean)
-    // console.log(loggedIn)
+    const auth = getAuth()
+    const [loggedIn, setLoggedIn] = useState(Boolean)
+    console.log(loggedIn)
 
-    // useEffect(() => {
-    //     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-    //         if (currentUser) {
-    //             setLoggedIn(true)
-    //         } else {
-    //             setLoggedIn(false)
-    //         }
-    //     });
-    //     return () => unsubscribe();
-    // }, []);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setLoggedIn(true)
+            } else {
+                setLoggedIn(false)
+            }
+        });
+        return () => unsubscribe();
+    }, []);
 
 //code for nav bar
-//{loggedIn && <NavBarPost /> }
-//{!loggedIn && <NavBarPre /> }
+{loggedIn && <NavBarPost /> }
+{!loggedIn && <NavBarPre /> }
 
 
     return (
         <div className='animate__fadeIn animate__animated fade c'>
             <div className="background"><img src={"/images/faqBackground.svg"} class="background-img" /></div>
-            <NavBarPre /> 
+            {loggedIn && <NavBarPost /> }
+            {!loggedIn && <NavBarPre /> }
                 <div className="page-header-centered">
                     <div className="title">Frequently Asked Questions</div>
                 </div>
